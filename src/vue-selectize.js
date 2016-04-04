55ac44c02f22d0f413c6b231f8c52832926d3169
Martyn Ling <mling@str8-4ward.com>
2016-04-04 18:08:16 +0100

    Added missing install method so can be used as a Vue plugin.

--- src/vue-selectize.js
+++ src/vue-selectize.js
@@ -1,36 +1,41 @@
-Vue.directive('selectize', {
-    twoWay: true,
-    priority: 1000,
 
-    params: [
-        'options', 'settings'
-    ],
+function install(Vue, options) {
 
-    bind: function () {
-        var self = this
-        var params = this.params.settings;
-        if (this.params.options) {
-            params.options = this.params.options;
-        }
-        $(this.el)
-            .selectize(params)
-            .on('change', function (e) {
-                // if multi-select enabled, value is a delimited string of selectedOptions values
-                if (this.selectize.settings.mode == 'multi'){
-                    var values = [];
-                    for (var i=0;i<this.selectedOptions.length;i++){
-                        values.push(this.selectedOptions[i].value);
+    Vue.directive('selectize', {
+        twoWay: true,
+        priority: 1000,
+
+        params: [
+            'options', 'settings'
+        ],
+
+        bind: function () {
+            var self = this
+            var params = this.params.settings;
+            if (this.params.options) {
+                params.options = this.params.options;
+            }
+            $(this.el)
+                .selectize(params)
+                .on('change', function (e) {
+                    // if multi-select enabled, value is a delimited string of selectedOptions values
+                    if (this.selectize.settings.mode == 'multi'){
+                        var values = [];
+                        for (var i=0;i<this.selectedOptions.length;i++){
+                            values.push(this.selectedOptions[i].value);
+                        }
+                        self.set(values.join(this.selectize.settings.delimiter));
+                    } else {
+                        self.set(this.value);
                     }
-                    self.set(values.join(this.selectize.settings.delimiter));
-                } else {
-                    self.set(this.value);
-                }
-            })
-    },
-    update: function (value) {
-        $(this.el).val(value).trigger('change')
-    },
-    unbind: function () {
-        $(this.el).off().selectize('destroy')
-    }
-});
+                })
+        },
+        update: function (value) {
+            $(this.el).val(value).trigger('change')
+        },
+        unbind: function () {
+            $(this.el).off().selectize('destroy')
+        }
+    });
+
+}
