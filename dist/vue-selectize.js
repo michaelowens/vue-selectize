6d9cb49885d78419083b150a865bd8b57b9ca751
Martyn Ling <mling@str8-4ward.com>
2016-04-04 15:57:37 +0100

    vue-selectize for Vue.js v1.

--- dist/vue-selectize.js
+++ dist/vue-selectize.js
@@ -1,102 +1,91 @@
 /**
- * vue-selectize v0.0.3
+ * vue-selectize v1.0.0
  * 
  * Copyright (c)  Michael Owens, contributors.
  * Licensed under the ISC license.
  */
-(function(root, factory){
-    var selectize = {
-        twoWay: true,
+;(function () {
+    var VueSelectize = {};
+    var selectize = typeof require === 'function'
+        ? require('selectize')
+        : window.selectize
 
-        selectizeSettings: {},
+    if (!selectize) {
+        throw new Error('[vue-selectize] cannot locate selectize.')
+    }
 
-        bind: function () {
-            var optionsExpression = this.el.getAttribute('options'),
-                settingsExpression = this.el.getAttribute('settings'),
-                self = this,
-                optionsData;
+    VueSelectize.settings = {}
 
-            if (optionsExpression) {
-                optionsData = this.vm.$eval(optionsExpression);
-                this.vm.$watch(optionsExpression, this.optionsChange.bind(this));
+    VueSelectize.install = function(Vue, settings) {
+
+        Vue.directive('selectize', {
+            twoWay: true,
+            priority: 1000,
+
+            params: [
+                'options', 'settings'
+            ],
+
+            bind: function () {
+                var self = this
+                // Settings are set in the following order with the latter values overriding the former:
+                // 1. Passed with Vue.use
+                // 2. Set in Javascript
+                // 3. Set on the select element
+                var params = {}
+                params = mergeData(params, settings)
+                params = mergeData(params, VueSelectize.settings)
+                params = mergeData(params, this.params.settings)
+                // Options set on select element override those in settings
+                if (this.params.options) {
+                    params.options = this.params.options
+                }
+                $(this.el)
+                    .selectize(params)
+                    .on('change', function (e) {
+                        // if multi-select enabled, value is a delimited string of selectedOptions values
+                        if (this.selectize.settings.mode == 'multi') {
+                            var values = [];
+                            for (var i = 0; i < this.selectedOptions.length; i++) {
+                                values.push(this.selectedOptions[i].value)
+                            }
+                            self.set(values.join(this.selectize.settings.delimiter))
+                        } else {
+                            self.set(this.value)
+                        }
+                    })
+            },
+            update: function (value) {
+                $(this.el).val(value).trigger('change')
+            },
+            unbind: function () {
+                $(this.el).off().selectize('destroy')
             }
+        });
 
-            this.selectizeSettings = {
-                options: optionsData,
-                onChange: function (value) {
-                    self.set(value);
-                    self.nativeEvent('change').call();
-                },
-                onFocus: this.nativeEvent('focus').bind(this),
-                onBlur: this.nativeEvent('blur').bind(this)
-            };
+    }
 
-            if (settingsExpression) {
-                var userSettings = this.vm.$eval(settingsExpression);
-                this.selectizeSettings = $.extend({}, this.selectizeSettings, userSettings);
-                this.vm.$watch(settingsExpression, this.settingsChange.bind(this), {
-                    deep: true
-                });
+    function mergeData(to, from) {
+        var key, toVal, fromVal;
+        for (key in from) {
+            toVal = to[key];
+            fromVal = from[key];
+            if (Vue.util.isObject(toVal) && Vue.util.isObject(fromVal)) {
+                mergeData(toVal, fromVal);
+            } else {
+                Vue.util.set(to, key, fromVal);
             }
-
-            $(this.el).selectize(this.selectizeSettings);
-        },
-
-        nativeEvent: function (eventName) {
-            var self = this;
-            return function () {
-                var event = new Event(eventName);
-                self.el.dispatchEvent(event);
-            };
-        },
-
-        optionsChange: function (options) {
-            var selectize = this.el.selectize,
-                value = this.el.selectize.getValue();
-
-            selectize.clearOptions();
-            selectize.addOption(options);
-            selectize.refreshOptions(false);
-            selectize.setValue(value);
-        },
-
-        settingsChange: function (settings) {
-            var value = this.el.selectize.getValue();
-
-            this.selectizeSettings = $.extend({}, this.selectizeSettings, settings);
-
-            this.el.selectize.destroy();
-            $(this.el).selectize(this.selectizeSettings);
-            this.el.selectize.setValue(value);
-        },
-
-        update: function (value) {
-            this.el.selectize.setValue(value);
-        },
-
-        unbind: function () {
-            this.el.selectize.destroy();
         }
-    };
-
-    if (typeof exports === 'object' && typeof module === 'object') {
-        module.exports = factory();
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    }
-    else if (typeof exports === 'object') {
-        exports['vue-selectize'] = factory();
-    }
-    else {
-        root['vue-selectize'] = factory();
+        return to;
     }
 
-    function factory() {
-        return function (Vue, options) {
-            options = options || {};
-            var directiveName = options.directive || 'selectize';
-            Vue.directive(directiveName, selectize);
-        };
+    if (typeof exports == "object") {
+        module.exports = VueSelectize
+    } else if (typeof define == "function" && define.amd) {
+        define([], function(){ return VueSelectize })
+    } else if (window.Vue) {
+        window.VueSelectize = VueSelectize
+        Vue.use(VueSelectize)
     }
-})(this);
+
+})()
